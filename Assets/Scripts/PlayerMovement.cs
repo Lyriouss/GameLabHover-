09851d037fc1e5b92e77aea4249a2b2f34094659
Vyles <Vyles17@gmail.com>
2026-02-27 01:30:54 +0100

    Finito lo script di movimento :D
    Ora riesce a salire le scale, ho fixato la rotazione quando si preme A o D, e frena dopo l'accelerata. Evviva!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,37 +1,42 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody playerRB;
-    private float moveHorizontal;
     private float moveForward;
 
     [Header("Player Movement Parameters")]
     [SerializeField] public float accelerationForce = 10f; //la forza limite dell'accelerazione 
     [SerializeField] public float maxSpeed = 25f; //velocità massima di movimento del Player
     [SerializeField] private float rotationSpeed = 20f; //la velocità con cui il player ruota a sx/dx
+    [SerializeField] private float groundDistance = 3f; // distanza fra player e ground
+
+    public LayerMask groundLayer;
+    public float rayLenght = 10f;
 
 
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+
     }
 
-    public void Update()
-    {
-        moveHorizontal = Input.GetAxis("Horizontal");
-        moveForward = Input.GetAxis("Vertical");
-    }
 
     public void FixedUpdate()
     {
         Accelerate();
         Rotate();
+        CheckGround();
     }
+
+
 
     //per l'accelerazione avanti/indietro
     void Accelerate()
     {
+        moveForward = Input.GetAxis("Vertical");
+
         if (moveForward != 0)
         {
             playerRB.AddForce(transform.forward * moveForward * accelerationForce, ForceMode.Acceleration);
@@ -42,16 +47,59 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
+        //per farlo frenare:
+        
+        float decelerationTime = 1f; // tempo che ci mette per fermarsi dopo aver smesso di ricevere l'input di accelerazione
+        float decelerationRate = playerRB.linearVelocity.magnitude / decelerationTime; //e il suo rate
+
+        playerRB.linearVelocity = Vector3.MoveTowards(playerRB.linearVelocity, Vector3.zero, decelerationRate * Time.fixedDeltaTime);//applicato al fixed DeltaTime
+
     }
+
     //per ruotare il player a destra/sinistra
     void Rotate()
     {
-        float rotation = moveHorizontal * rotationSpeed * Time.fixedDeltaTime;
+        float rotation = rotationSpeed * Time.fixedDeltaTime;
 
-        if (moveHorizontal != 0)
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRB.MoveRotation(playerRB.rotation * Quaternion.Euler(0f, -rotation, 0f));
+        }
+
+        else if (Input.GetKey(KeyCode.D))
         {
             playerRB.MoveRotation(playerRB.rotation * Quaternion.Euler(0f, rotation, 0f));
         }
 
+    }
+
+    //per tenere il player sempre a un tot di distanza dal ground, sia questo ground o scale
+    void CheckGround()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(playerRB.position, Vector3.down, out hit, groundDistance, groundLayer))
+        {
+            //tocca disattivare la gravità sennò rimbalza all'infinito lol
+            playerRB.useGravity = false;
+
+            float difference = groundDistance - hit.distance;
+
+            playerRB.position += new Vector3(0, difference, 0);
+        }
+
+        else
+        {
+            //la attivo solo quando cadiamo
+            playerRB.useGravity = true;
+        }
+
+    }
+
+    //per colorare il raycast in fase di develop
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Vector3.down * groundDistance);
     }
 }
