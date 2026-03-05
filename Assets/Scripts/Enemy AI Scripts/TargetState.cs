36380e093a23cc1bf6ec0d70ffb6cb6e6f1ec70d
Luca Benjamin Kleis <lucakleis@gmail.com>
2026-03-05 09:28:56 +0100

    Reapply "Merge remote-tracking branch 'origin/Feature' into Feature"
    This reverts commit d105f7fb523e4b69af4da7414007ab18a17e0402.

---
+++ Assets/Scripts/Enemy AI Scripts/TargetState.cs
@@ -0,0 +1,49 @@
+public class TargetState : IEnemyState, IEnemyMovement
+{
+    EnemyManager em;
+
+    public TargetState(EnemyManager state)
+    {
+        em = state;
+    }
+
+    public void Enter()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+
+    }
+
+    public void CheckForTarget()
+    {
+
+    }
+
+    public void CalibrateVehicle()
+    {
+
+    }
+
+    public void Accelerate()
+    {
+
+    }
+
+    public void CheckGround()
+    {
+
+    }
+}
